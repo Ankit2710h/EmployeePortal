a8bff1b4936ad2247d2a12366de6e3d24576a602
Ankit Hatwal <ankit.s.hatwal@boeing.com>
2019-08-12 17:51:39 +0530

    Use EmployeeService object in Employee controller

--- src/main/java/Employee_Portal/service/EmployeeService.java
+++ src/main/java/Employee_Portal/service/EmployeeService.java
@@ -3,6 +3,7 @@ package Employee_Portal.service;
 import Employee_Portal.model.Employee;
 import Employee_Portal.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import javax.validation.Valid;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
