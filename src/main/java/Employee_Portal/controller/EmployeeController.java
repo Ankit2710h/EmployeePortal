c2abde0b4a8b765a127161108bfe725db4b0a15e
Ankit Hatwal <ankit.s.hatwal@boeing.com>
2019-08-12 15:27:45 +0530

    Use EmployeeService object in Employee controller

--- src/main/java/Employee_Portal/controller/EmployeeController.java
+++ src/main/java/Employee_Portal/controller/EmployeeController.java
@@ -1,37 +1,36 @@
 package Employee_Portal.controller;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import Employee_Portal.model.Employee;
+import Employee_Portal.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import Employee_Portal.repository.EmployeeRepository;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
-        List<Employee> list= employeeRepository.findAll();
-        Collections.sort(list);
+        List<Employee> list = employeeService.getAllEmployees();
         return list;
     }
 
     @PostMapping("/employees")
     public Employee createEmployee(@Valid @RequestBody Employee employee) {
-        System.out.println(employee);
-        return employeeRepository.save(employee);
+        return employeeService.createEmployee(employee);
     }
 }
 
